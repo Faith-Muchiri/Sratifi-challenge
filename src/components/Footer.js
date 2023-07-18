edf8e041e945d3802b4d0d245bed3cc28bd308ab
Faith <faithwambui891@gmail.com>
2023-07-18 16:40:41 +0300

    solve deployment issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,14 @@ const Footer = () => {
   return (
     <div className="mt-auto p-4 text-center bg-gray-200">
       <p className="text-black">
-        created by <a href="#" className="text-gray-600">Faith Wambui Muchiri</a> - devChallenges.io
+        created by{' '}
+        <a
+          href="https://www.linkedin.com/in/faith-wambui-454533216/" 
+          className="text-gray-600"
+        >
+          Faith Wambui Muchiri
+        </a>{' '}
+        - devChallenges.io
       </p>
     </div>
   );
