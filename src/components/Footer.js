9b59708bacc62dad4529893a4651262015a13397
Faith <faithwambui891@gmail.com>
2023-07-18 16:06:55 +0300

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="mt-auto p-4 text-center bg-gray-200">
+      <p className="text-black">
+        created by <a href="#" className="text-gray-600">Faith Wambui Muchiri</a> - devChallenges.io
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
